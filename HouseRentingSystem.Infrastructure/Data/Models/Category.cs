c10f0d947c7a7dc47bcc6b11626ce659704c983f
PetyaIvovaYakimova <petya.yakimova@amexis.net>
2024-02-24 14:53:39 +0200

    Add project files.

---
+++ HouseRentingSystem.Infrastructure/Data/Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using static HouseRentingSystem.Infrastructure.Constants.DataConstants;
+
+namespace HouseRentingSystem.Infrastructure.Data.Models
+{
+	[Comment("House category")]
+	public class Category
+	{
+		[Key]
+		[Comment("Category Identifier")]
+		public int Id { get; init; }
+
+		[Required]
+		[MaxLength(CategoryNameMaxLength)]
+		[Comment("Category Name")]
+		public string Name { get; set; } = string.Empty;
+
+		public IEnumerable<House> Houses { get; init; } = new List<House>();
+	}
+}
---
+++ HouseRentingSystem.Infrastructure/Data/Models/House.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using static HouseRentingSystem.Infrastructure.Constants.DataConstants;
+
+namespace HouseRentingSystem.Infrastructure.Data.Models
+{
+	[Comment("House to rent")]
+	public class House
+	{
+		[Key]
+		[Comment("House Identifier")]
+		public int Id { get; set; }
+
+		[Required]
+		[StringLength(HouseTitleMaxLength)]
+		[Comment("House Title")]
+		public string Title { get; set; } = string.Empty;
+
+		[Required]
+		[StringLength(HouseAddressMaxLength)]
+		[Comment("House Address")]
+		public string Address { get; set; } = string.Empty;
+
+		[Required]
+		[StringLength(HouseDescriptionMaxLength)]
+		[Comment("House Description")]
+		public string Description { get; set; } = string.Empty;
+
+		[Required]
+		[Comment("House Image URL")]
+		public string ImageUrl { get; set; } = string.Empty;
+
+		[Required]
+		[Column(TypeName = "decimal(18,2)")]
+		[Comment("House Price per Month")]
+		//[Range(typeof(decimal), HousePricePerMonthMin, HousePricePerMonthMax, ConvertValueInInvariantCulture = true)]
+		public decimal PricePerMonth { get; set; }
+
+		[Required]
+		[Comment("Category Identifier")]
+		public int CategoryId { get; set; }
+
+		[ForeignKey(nameof(CategoryId))]
+		public Category Category { get; set; } = null!;
+
+		[Required]
+		[Comment("Agnet Identifier")]
+		public int AgentId { get; set; }
+
+		[ForeignKey(nameof(AgentId))]
+		public Agent Agent { get; set; } = null!;
+
+		[Comment("User identifier of the renter")]
+		public string? RenterId { get; set; }
+	}
+}
