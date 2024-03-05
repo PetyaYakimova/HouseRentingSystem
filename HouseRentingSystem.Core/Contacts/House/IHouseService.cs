df804ad5fd2b5b603fea6a54dbd1f1ed47d67dff
PetyaYakimova <petya_yakimova@abv.bg>
2024-03-05 10:02:34 +0200

    Add repository to work with the db, add house service.

---
+++ HouseRentingSystem.Core/Contacts/House/IHouseService.cs
@@ -0,0 +1,9 @@
+﻿using HouseRentingSystem.Core.Models.House;
+
+namespace HouseRentingSystem.Core.Contacts.House
+{
+	public interface IHouseService
+	{
+		Task<IEnumerable<HouseIndexServiceModel>> LastThreeHousesAsync();
+	}
+}
--- HouseRentingSystem/Extensions/ServiceCollectionExtension.cs
+++ HouseRentingSystem/Extensions/ServiceCollectionExtension.cs
@@ -1,4 +1,7 @@
-﻿using HouseRentingSystem.Infrastructure.Data;
+﻿using HouseRentingSystem.Core.Contacts.House;
+using HouseRentingSystem.Core.Services.House;
+using HouseRentingSystem.Infrastructure.Data;
+using HouseRentingSystem.Infrastructure.Data.Common;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,6 +11,7 @@ namespace Microsoft.Extensions.DependencyInjection
 	{
 		public static IServiceCollection AddApplicationServices(this IServiceCollection services)
 		{
+			services.AddScoped<IHouseService, HouseService>();
 			return services;
 		}
 
@@ -16,6 +20,8 @@ namespace Microsoft.Extensions.DependencyInjection
 			var connectionString = config.GetConnectionString("DefaultConnection");
 			services.AddDbContext<HouseRentingDbContext>(options =>
 				options.UseSqlServer(connectionString));
+
+			services.AddScoped<IRepository, Repository>();
 
 			services.AddDatabaseDeveloperPageExceptionFilter();
 
