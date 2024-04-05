97661c883d142a83453b23096b22e928a9af4cfb
PetyaYakimova <petya_yakimova@abv.bg>
2024-04-05 10:28:34 +0300

    Add caching

--- HouseRentingSystem.Core/Constants/AdministratorConstants.cs
+++ HouseRentingSystem.Core/Constants/AdministratorConstants.cs
@@ -7,5 +7,7 @@
 		public const string AdminAreaName = "Admin";
 
 		public const string AdminEmail = "admin@mail.com";
+
+		public const string UsersCacheKey = "UsersCacheKey";
 	}
 }
--- HouseRentingSystem/Program.cs
+++ HouseRentingSystem/Program.cs
@@ -14,6 +14,7 @@ builder.Services.AddControllersWithViews(options =>
 });
 
 builder.Services.AddApplicationServices();
+builder.Services.AddMemoryCache();
 
 var app = builder.Build();
 
